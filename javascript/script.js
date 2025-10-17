a36678373a91888a9c491c813801180d8a1b2607
prasuna-658 <prasunagnana658@gmail.com>
2025-10-17 12:19:07 +0530

    script file

---
+++ javascript/script.js
@@ -0,0 +1,48 @@
+//let UserName="prasuna"
+//let Password="prasuna@7"
+//let Email="prasuna@gmail.com"
+//let DOB="18-03-05"
+//let Gender="female"
+//let Feedback="Good"
+
+//console.log(UserName)
+//console.log(Password)
+//console.log(Email)
+
+//document.write(UserName)
+
+//primitive datatypes
+var stdname="prasuu"
+console.log(stdname)
+console.log(typeof(stdname))
+var age="45"
+console.log(typeof(age))
+var b=null
+console.log(typeof(b))
+var c=true
+console.log(typeof(c))
+var address= undefined
+console.log(typeof(address))
+
+//non primitive datatypes
+var stddata=[23,45,67,89,10]
+console.log(stddata.length)
+console.log(stddata[1])
+stddata[3]=76
+console.log(stddata[3])
+
+const emp_data={
+    empid:34,
+    empname:"prasuu",
+    empage:21,
+    empaddress:"banglore"
+}
+console.log(emp_data)
+
+
+//const std_data=[{
+   //
+//console.log(std_data[0].stddept)
+//console.log(std_data)
+
+
