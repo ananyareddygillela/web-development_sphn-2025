8ed2de62acc4c248f53324e02e99b8a6fb776240
praveenv262006-cyber/praveen-519 <thanusreer2005@gmail.com>
2025-10-17 12:23:37 +0530

    script file

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,35 @@
+var stdname = "john"
+console.log(stdname)
+console.log(typeof(stdname))
+var age ="67"
+console.log(age)
+console.log(typeof(age))
+var doy = 2000
+console.log(doy)
+console.log(typeof(doy))
+var dept = "null"
+console.log(null)
+console.log(typeof(null))
+var citizenship = true
+console.log(citizenship)
+console.log(typeof(citizenship))
+var address = undefined
+console.log(address)
+console.log(typeof(address))
+// non primitive
+var studata = [23,45,67,89]
+console.log(studata.length)
+console.log(studata[0])
+console.log(studata[3])
+console.log(studata.slice(0,2))
+
+const Emp_data = {
+    empid:23,
+    empname:"Mahesh",
+    empage:45,
+    empaddress:"hyd"
+}
+console.log(Emp_data.empname,Emp_data.empaddress)
+
+
+console.log(std_data[1].stdname,std_data[1].stdaddress)
